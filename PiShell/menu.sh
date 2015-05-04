84731b970e6a0414674d04e639dfc10936c0ffd8
jsalmen <joakim,salmen@ha.ax>
2015-05-04 09:50:45 +0000

    Added deamon selection to menu

--- PiShell/menu.sh
+++ PiShell/menu.sh
@@ -3,13 +3,14 @@
     # PiShell menu
     
     printf "\n$PI_SHELL_CLR_ASK\nWhat do you want to do?\n$PI_SHELL_CLR_OPT"
-    select option in "Install" "Update" "Test" "Build" "Quit"
+    select option in "Install" "Update" "Test" "Build" "Deamon" "Quit"
     do
         case $option in
             "Install" ) ./install.sh; break;;
             "Update" ) ./update.sh; break;;
             "Test" ) ./test.sh; break;;
             "Build" ) ./build.sh; break;;
+            "Deamon" ) ./deamon.sh; break;;
             "Quit" ) break;;
         esac
     done
