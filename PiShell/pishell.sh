9b1b89fd9dd2207ac3e79de6adaea4e9d7499667
jsalmen <joakim,salmen@ha.ax>
2015-05-21 12:27:11 +0000

    added moved pishell.sh

---
+++ PiShell/pishell.sh
@@ -0,0 +1,29 @@
+  
+    #      Aland Sailing Robot       #
+    # RaspberryPi Management Scripts #
+    #--------------------------------#
+    # Main script
+
+    # Config script
+    source PiShell/config.sh
+    
+    # Show welcome message
+    printf "\n$PI_SHELL_CLR_HEAD"
+    printf "\n     *****************************************************"
+    printf "\n     *                                                   *"
+    printf "\n     *           .:          .:: ::    .:::::::          *"
+    printf "\n     *          .: ::      .::    .::   ::    .::        *"
+    printf "\n     *         .:  .::      .::         ::    .::        *"
+    printf "\n     *        .::   .::       .::       : .::            *"
+    printf "\n     *       .:::::: .::         .::    ::  .::          *"
+    printf "\n     *      .::       .::  .::    .::   ::    .::        *"
+    printf "\n     *     .::         .::   .:: ::    .::      .::      *"
+    printf "\n     *                                                   *"
+    printf "\n     *                 Aland Sailing Robot               *"
+    printf "\n     *                  RaspberryPi Shell                *"
+    printf "\n     *****************************************************"
+    printf "$PI_SHELL_CLR_INFO"
+    
+    # Launch menu
+    cd $PI_SHELL_PATH
+    ./menu.sh
