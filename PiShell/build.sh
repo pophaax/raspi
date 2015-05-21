ecb2de7c366e40c53312eceda32f98aecf9d8809
jsalmen <joakim,salmen@ha.ax>
2015-05-21 13:06:02 +0000

    updated script paths in build.sh

--- PiShell/build.sh
+++ PiShell/build.sh
@@ -4,28 +4,19 @@
     #--------------------------------#
     # Make binaries
     
-   source repos.sh
+    source repos.sh
     
-    print_status()
-    {
-        if $1
-        then
-            printf "$PI_SHELL_CLR_OK"
-            printf "OK\n"
-        else
-            printf "$PI_SHELL_CLR_ERR"
-            printf "ERROR\n"
-        fi
-    }
-
     cd $INSTALLATION_PATH$SAILBOT_REPO_MAIN
-    for MODULE in ${SAILBOT_REPO_MODULES[@]}
-        do
-            cd $MODULE
-            printf "$PI_SHELL_CLR_INFO\nMaking tests for module $PI_SHELL_CLR_DIR$MODULE in $PI_SHELL_CLR_DIR$INSTALLATION_PATH$SAILBOT_REPO_MAIN/$MODULE/$PI_SHELL_CLR_INFO\n"
-            print_status "make"
-            cd ..
-        done
+    printf "$PI_SHELL_CLR_INFO\nBuilding main executable in $PI_SHELL_CLR_DIR$INSTALLATION_PATH$SAILBOT_REPO_MAIN/$PI_SHELL_CLR_INFO\n"
+
+    if "make"
+    then
+        printf "$PI_SHELL_CLR_OK"
+        printf "OK\n"
+    else
+        printf "$PI_SHELL_CLR_ERR"
+        printf "ERROR\n"
+    fi
 
     # Return to menu
     cd $PI_SHELL_PATH
