56010dbaaa6be6314d8d8b60b38325efecff137b
jsalmen <joakim,salmen@ha.ax>
2015-05-21 13:07:30 +0000

    updated script paths in build.sh

--- PiShell/build.sh
+++ PiShell/build.sh
@@ -6,7 +6,7 @@
     
     source repos.sh
     
-    cd $INSTALLATION_PATH$SAILBOT_REPO_MAIN
+    cd $INSTALLATION_PATH/$SAILBOT_REPO_MAIN
     printf "$PI_SHELL_CLR_INFO\nBuilding main executable in $PI_SHELL_CLR_DIR$INSTALLATION_PATH/$SAILBOT_REPO_MAIN/$PI_SHELL_CLR_INFO\n"
 
     if "make"
